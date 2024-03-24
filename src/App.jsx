069cf83eee8cd6b26f3a8ed2bd1ab04f12eb9b39
Sowmya Malur <smalur.github@gmail.com>
2024-03-24 01:24:04 -0700

    Add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from './components/Header/Header';
+import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
 import './App.scss';
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
        
     </div>
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return(
+    <footer>
+        <p>&copy;UrbanEaseSpot. All rights reserved.</p>
+    </footer>);
+}
+
+export default Footer;
