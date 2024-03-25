562d85afc63492e10a1d9b87db392d49a19992f7
Sowmya Malur <smalur.github@gmail.com>
2024-03-24 21:51:57 -0700

    Add basic mapcontainer

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,26 @@
+import "../HomePage/HomePage.scss"
+import 'leaflet/dist/leaflet.css'
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import MarkerClusterGroup from "react-leaflet-cluster";
+import { Icon, divIcon, point } from "leaflet";
+
 function HomePage() {
-    return(<>
-    <p>This is home page - add map code here</p>
-    </>);
+    return(<main>
+    <MapContainer
+      className="full-height-map"
+      center={[49.26328581657055, -123.13380465835546]}
+      // center={[49.2827, -123.1207]} //this is working
+      zoom={13}
+      minZoom={3}
+      maxZoom={19}
+      maxBounds={[[-85.06, -180], [85.06, 180]]}
+      scrollWheelZoom={true}>
+     <TileLayer
+       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>    
+    </main>);
 }
 
 export default HomePage;
