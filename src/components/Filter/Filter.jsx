798976062a6858a1763bb52009b42ab5d8293aec
Sowmya Malur <smalur.github@gmail.com>
2024-03-25 19:28:17 -0700

    Add checkbox for payment

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,36 +1,52 @@
 import { useNavigate, useParams } from "react-router-dom";
+
+// Import icons
 import back from "../../assets/icons/round_arrow_back_black_24dp.png";
 import payment from "../../assets/icons/round_payment_black_24dp.png";
+import motorbike from "../../assets/icons/round_two_wheeler_black_24dp.png";
+import accessible from "../../assets/icons/round_accessible_forward_black_24dp.png";
+import checkBoxSelected from "../../assets/icons/round_check_box_black_24dp.png";
+import checkBoxEmpty from "../../assets/icons/round_check_box_outline_blank_black_24dp.png";
 
 function Filter({ handleClick, handleFilterOptions }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const sendDataToHomePage = () => {
-        // Example data
-        const filterOptions = {
-          acceptCreditCard: true,
-        };
-        // Call the callback function with the filter data
-        handleFilterOptions(filterOptions);
-      };
+  const sendDataToHomePage = () => {
+    // Example data
+    const filterOptions = {
+      acceptCreditCard: true,
+    };
+    // Call the callback function with the filter data
+    handleFilterOptions(filterOptions);
+  };
 
-    return(
-        <>
-        <img 
-        src={back}
-        onClick={() => { 
-            handleClick(false); 
-            navigate("/")}}
-        alt="back-icon"/>
+  return (
+    <>
+      <div>
+        <img
+          src={back}
+          onClick={() => {
+            handleClick(false);
+            navigate("/");
+          }}
+          alt="back-icon"
+        />
         <p>Filter</p>
-        <img 
-        src={payment} 
-        alt="payment-icon"
-        onClick={sendDataToHomePage}/>
-        Show spots that accept credit card
-        </>
-        
-    );
+      </div>
+      <input
+        type="checkbox"
+        id="acceptCreditCard"
+        style={{ display: "none" }} // hide the default checkbox. TODO: in scss
+      />
+      <label htmlFor="acceptCreditCard">
+        <div>
+          <img src={payment} alt="payment-icon" onClick={sendDataToHomePage} />
+          Show spots that accept credit card
+        </div>  
+        <img src={checkBoxSelected} alt="check-box-selected" />
+      </label>
+    </>
+  );
 }
 
 export default Filter;
