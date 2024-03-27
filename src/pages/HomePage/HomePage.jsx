ad6edcb006cf445532edac8d9807df596fdbd95e
Sowmya Malur <smalur.github@gmail.com>
2024-03-26 17:25:02 -0700

    Add connection to backend using axios and useEffect

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,9 +4,13 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { Icon, divIcon, point } from "leaflet";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
 import filter from "../../assets/icons/round_tune_black_24dp.png";
+
+// Import components
 import ParkingDuration from "../../components/ParkingDuration/ParkingDuration";
 import Filter from "../../components/Filter/Filter";
 
@@ -28,15 +32,31 @@ const createClusterCustomIcon = function (cluster) {
 function HomePage() {
   const [showComponent, setShowComponent] = useState(false);
   const [filterOptions, setFilterOptions] = useState({});
+  const [parkingMeters, setParkingMeters] = useState([]);
 
   const navigate = useNavigate();
+
+  // Call axios to get parking meter data
+  useEffect(() => {
+    const getAllParkingMeters = async () => {
+      // TODO: test why env varaible is not working
+      // const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/parking`);
+      const response = await axios.get("http://localhost:8080/api/parking");
+      console.log("response", response);
+      if (response.data && response.status === 200) {
+        setParkingMeters(response.data);
+      }
+    };
+
+    getAllParkingMeters();
+  }, []);
+
   const handleClick = () => {
-    // navigate("/parking");
     setShowComponent("parking-duration");
   };
 
   const handleFilterOptions = (data) => {
-    setFilterOptions(data)
+    setFilterOptions(data);
     setShowComponent(false);
   };
 
@@ -46,12 +66,14 @@ function HomePage() {
         <>
           <section>
             <input type="search"></input>
-            <img 
-            src={filter} 
-            alt="filter-icon" 
-            width={24} 
-            height={24} 
-            onClick={()=>{setShowComponent("filter")}}
+            <img
+              src={filter}
+              alt="filter-icon"
+              width={24}
+              height={24}
+              onClick={() => {
+                setShowComponent("filter");
+              }}
             />
           </section>
           <MapContainer
@@ -74,7 +96,30 @@ function HomePage() {
               chunkedLoading
               iconCreateFunction={createClusterCustomIcon}
             >
-              <Marker
+              {!parkingMeters
+                ? ""
+                : parkingMeters.map((parking) => {
+                    // console.log(parking.geo_point_2d.lat);
+                    return (
+                      <Marker
+                        position={[
+                          parking.geo_point_2d.lat,
+                          parking.geo_point_2d.lon,
+                        ]}
+                        icon={customIcon}
+                      >
+                        <Popup>
+                          <div>
+                            <p>{parking.meterhead}</p>
+                            <button onClick={handleClick}>More Details</button>
+                          </div>
+                        </Popup>
+                      </Marker>
+                    );
+                  })}
+
+              {/* Hardcoded working code */}
+              {/* <Marker
                 position={[49.26328581657055, -123.13380465835546]}
                 icon={customIcon}
               >
@@ -103,15 +148,15 @@ function HomePage() {
                     <button onClick={handleClick}>More Details</button>
                   </div>
                 </Popup>
-              </Marker>
+              </Marker> */}
             </MarkerClusterGroup>
           </MapContainer>
           {filterOptions && (
-        <div>
-          <p>Filter Options:</p>
-          <pre>{JSON.stringify(filterOptions, null, 2)}</pre>
-        </div>
-      )}
+            <div>
+              <p>Filter Options:</p>
+              <pre>{JSON.stringify(filterOptions, null, 2)}</pre>
+            </div>
+          )}
         </>
       )}
 
@@ -119,12 +164,13 @@ function HomePage() {
       {showComponent === "parking-duration" && (
         // <ParkingDuration handleClick={() => setShowComponent(false)} />
         <ParkingDuration handleClick={handleClick} />
-
       )}
-        {showComponent === "filter" && (
-        <Filter handleClick={() => setShowComponent(false)} handleFilterOptions={handleFilterOptions}/>
+      {showComponent === "filter" && (
+        <Filter
+          handleClick={() => setShowComponent(false)}
+          handleFilterOptions={handleFilterOptions}
+        />
       )}
-
     </main>
   );
 }
