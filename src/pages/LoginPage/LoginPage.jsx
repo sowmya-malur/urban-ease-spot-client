e4ab061617b7270978db9c0e429226fa7d22370e
Sowmya Malur <smalur.github@gmail.com>
2024-03-27 16:16:53 -0700

    Add event handlers and form validations to the login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,41 +1,111 @@
-
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import axios from "axios";
 
 function LoginPage() {
-    const navigate = useNavigate();
-    const errorMessage = "This field is required";
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState({});
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // this or user info
-    const [userInfo, setUserInfo] = useState({});
+  const errorMessage = "This field is required";
 
-    return(
-        <>
-        <h1> Welcome Back!</h1>
-        <p>Login below or <Link to="/">create an account</Link></p>
-        <form>
-            <label htmlFor="email">Email</label>
-            <input 
-            type="email"
-            id="email"
-            name="email"
-            placeholder="johnsmith@example.com"
-            />
-            <label htmlFor="password">Password</label>
-            <input 
-            type="password"
-            id="password"
-            name="password"
-            />
-            <Link to ="/">Forgot Password</Link>
-        </form>
-        <button>Sign In</button>
-        <Link to="/">Create Account</Link>
-        </>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // this or user info
+  const [userInfo, setUserInfo] = useState({});
+
+  // Get reference to the form fields to focus on error entry
+  const formRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  // Event handler for form field changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    // Set state based on input field name
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+
+    // Clear errors and hide error message when the user enters a value
+    if (value.trim() !== "") {
+      setErrors({});
+    }
+  };
+
+  // Function to validate fields on submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Initialize form errors object
+    let formErrors = {};
+
+    // Check the values from the form. If empty, set the error message and set the focus.
+    if (email.trim() === "") {
+      emailRef.current.focus();
+      formErrors.email = errorMessage;
+    } else if (!/^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/.test(email.trim())) {
+      emailRef.current.focus();
+      formErrors.email = "Invalid email format. Ex: example@example.com";
+    }
+    if (password.trim() === "") {
+      password.current.focus();
+      formErrors.password = errorMessage;
+    }
+    // Add validation for password length?
+
+    // Update state with form errors
+    if (Object.keys(formErrors).length !== 0) {
+      setErrors(formErrors);
+    }
+
+    // If there are no errors, verify the user
+    if (Object.keys(formErrors).length === 0) {
+      // API call to validate the user in the backend
+      console.log("login successful");
+    }
+  };
+
+  return (
+    <>
+      <h1>Welcome Back!</h1>
+      <p>
+        Login below or <Link to="/">create an account</Link>
+      </p>
+      <form id="login" ref={formRef}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="johnsmith@example.com"
+          value={email}
+          ref={emailRef}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          ref={passwordRef}
+          onChange={handleChange}
+        />
+        {/* TODO: add eye icon inside the password field */}
+        <Link to="/">Forgot Password</Link>
+      </form>
+      <button onClick={handleSubmit}>Sign In</button>
+      <Link to="/">Create Account</Link>
+    </>
+  );
 }
 
 export default LoginPage;
