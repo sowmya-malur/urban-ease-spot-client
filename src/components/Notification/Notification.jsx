c897f75cce44daf1c87399cff4f3bc38fb95a933
Sowmya Malur <smalur.github@gmail.com>
2024-03-29 11:41:01 -0700

    Add styling to notifications page

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,58 +1,90 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+// Import styling
+import "../Notification/Notification.scss";
+
+// Import icons
 import warningIcon from "../../assets/icons/round_warning_amber_black_24dp.png";
 import backIcon from "../../assets/icons/round_arrow_back_black_24dp.png";
 
 function Notification({ handleEndSession }) {
+  // Initialize hooks
+  const navigate = useNavigate();
 
-    const[expireSoon, setExpireSoon] = useState(false);
-    const navigate = useNavigate();
-    return (
-        <main>
-            <img
-                src={backIcon}
-                onClick={() => {
-                  // handleClick(false);
-                  // setShowComponent("home-page");
-                  navigate("/");
-                }}
-                alt="back-icon"
+  // Initialize state variables
+  const [expireSoon, setExpireSoon] = useState(false);
+
+  return (
+    <main>
+      <section className="notification">
+        <div className="notification__page-header">
+          <img
+            className="notification__icon"
+            src={backIcon}
+            onClick={() => {
+              // handleClick(false);
+              // setShowComponent("home-page");
+              navigate("/");
+            }}
+            alt="back-icon"
+          />
+          <h1 className="notification__title">Notification</h1>
+        </div>
+        <div className="notification__meter-info">
+          <p>Meter #</p>
+          {/* parking.meterid */}
+          <h2 className="notification__sub-title">872503</h2>
+          {/* parking.location */}
+          <p>Fairview</p>
+        </div>
+        {expireSoon ? (
+          <>
+            <div className="notification__container notification__container--top-spacing">
+              <img
+                src={warningIcon}
+                alt="warning-icon"
+                className="notification__icon notification__icon--warning"
               />
-              <h1>Notification</h1>
-              <p>Meter #</p>
-              {/* parking.meterid */}
-              <h2>872503</h2>
-              {/* parking.location */}
-              <p>Fairview</p>
-
-            {expireSoon? (
-                <>
-                <div>
-                    <img 
-                    src={warningIcon}
-                    alt="warning-icon"/>
-                <p>Your parking will expire soon, at</p>
-                <p>March 19, 12:30 pm</p>
-                <p>Please remove your car to avoid penalties.</p>
+              <p className="notification__info">
+                Your parking will expire soon, at
+              </p>
+              <p className="notification__time">March 19, 12:30 pm</p>
+              <p className="notification__info">
+                Please remove your car to avoid penalties.
+              </p>
             </div>
-            <button onClick={handleEndSession}>End Parking Session</button>
-
-                </>
-            ) :
-            ( <>
-            <div>
-                <p>Your parking is set to expire at</p>
-                <p>March 19, 12:30 pm</p>
-                <p>We will notify you 15 minutes before your parking session expires.</p>
+            <div className="notification__wrapper">
+              <button className="notification__cta" onClick={handleEndSession}>
+                End Parking Session
+              </button>
             </div>
-            <button onClick={()=> navigate("/")}>Okay</button>
-            </>)
-}    
-        
-        </main>
-        
-    );
-    }
-    
+          </>
+        ) : (
+          <>
+            <div className="notification__container">
+              <p className="notification__info">
+                Your parking is set to expire at
+              </p>
+              <p className="notification__time">March 19, 12:30 pm</p>
+              <p className="notification__info">
+                We will notify you 15 minutes before your parking session
+                expires.
+              </p>
+            </div>
+            <div className="notification__wrapper">
+              <button
+                className="notification__cta"
+                onClick={() => navigate("/")}
+              >
+                Okay
+              </button>
+            </div>
+          </>
+        )}{" "}
+      </section>
+    </main>
+  );
+}
+
 export default Notification;
