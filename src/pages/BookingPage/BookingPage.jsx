035ada683305ce3505f16a9fd8628b53a66de2cd
Sowmya Malur <smalur.github@gmail.com>
2024-03-30 16:50:10 -0700

    Fix issue with dependecy on selectedparkingmeter to calculate maximum stay and total cost on mount

--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,13 +1,26 @@
+// Import libraries
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+
+// Import components
 import ParkingDuration from "../../components/ParkingDuration/ParkingDuration";
 
+/**
+ * 
+ * @param {setIsLoggedIn} setIsLoggedIn callback function to set isLoggedIn state variable
+ * @param {setUserId} setUserId callback function to set the user id state variable
+ * @returns {JSX.Element} ParkingDuration component
+ */
 function BookingPage({ setIsLoggedIn, setUserId}) {
+
+  const { userId } = useParams();
+
   useEffect(() => {
     console.log("in useeffect bookinpage");
     setIsLoggedIn(localStorage.getItem("isLoggedIn"));
   }, []);
 
-  return <ParkingDuration setIsLoggedIn={setIsLoggedIn} setUserId={setUserId}/>;
+  return <ParkingDuration setIsLoggedIn={setIsLoggedIn} setUserId={setUserId} userId={userId}/>;
 }
 
 export default BookingPage;
