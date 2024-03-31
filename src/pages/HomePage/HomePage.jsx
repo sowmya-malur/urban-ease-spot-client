717bbb449be04ac3cca494b1177376e7e251697f
Sowmya Malur <smalur.github@gmail.com>
2024-03-30 17:15:39 -0700

    Update popup to not show current rate if the status is not vacant, clean up comments

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -38,10 +38,11 @@ const createClusterCustomIcon = function (cluster) {
 };
 
 /**
- *
+ * @param {setIsLoggedIn} setIsLoggedIn callback function to set if user is logged in
+ * @param {userId} ?
  * @returns
  */
-function HomePage({ setIsLoggedIn , userId}) {
+function HomePage({ setIsLoggedIn, userId }) {
   // Initialize hooks
   const navigate = useNavigate();
 
@@ -54,10 +55,16 @@ function HomePage({ setIsLoggedIn , userId}) {
   const [filteredParkingMeters, setFilteredParkingMeters] = useState([]);
   const [selectedParkingMeter, setSelectedParkingMeter] = useState("");
 
+  // Initialize constants
+  const currentTimeStamp = Date.now();
+  const currentDate = new Date(currentTimeStamp);
+  const currentHours = currentDate.getHours();
+  const currentDay = currentDate.getDay();
+
   // Set isLoggedIn from the localStorage on mount
   useEffect(() => {
     console.log("in useeffect homepage"); // TODO: del
-    console.log("localstorage",localStorage.getItem("isLoggedIn"));
+    console.log("localstorage", localStorage.getItem("isLoggedIn"));
     setIsLoggedIn(localStorage.getItem("isLoggedIn"));
   }, []);
 
@@ -105,11 +112,7 @@ function HomePage({ setIsLoggedIn , userId}) {
     setFilteredParkingMeters(filterParkingMeters);
   }, [filterOptions, parkingMeters]);
 
-  const displayParkingRate = (parking, currentTimeStamp) => {
-    const currentDate = new Date(currentTimeStamp);
-    const currentHours = currentDate.getHours();
-    const currentDay = currentDate.getDay();
-
+  const displayParkingRate = (parking) => {
     // TODO: del after testing
     // let currentDay = 1;
     // let currentHours = 10;
@@ -120,52 +123,52 @@ function HomePage({ setIsLoggedIn , userId}) {
       rate: "",
     };
 
-    // If the day is Mon/Tue/Wed/Thurs/Fri
+    // Weekday: M-F
     if (currentDay >= 1 && currentDay <= 5) {
       parkingInfo.day = "Mon-Fri: ";
 
       if (currentHours >= 9 && currentHours < 18) {
-        // If the time is between 9 am and 6 pm
+        // M-F: Between 9 am and 6 pm
         parkingInfo.time = "9 am - 6 pm";
         parkingInfo.rate = "$" + parking.r_mf_9a_6;
       } else if (currentHours >= 18 && currentHours < 22) {
-        // If the time is between 6 pm and 10 pm
+        // M-F: Between 6 pm and 10 pm
         parkingInfo.time = "6 pm - 10 pm";
         parkingInfo.rate = "$" + parking.r_mf_6p_10;
       } else {
-        // If the time is between 10 pm and 9 am
+        // M-F: Between 10 pm and 9 am
         parkingInfo.time = "10pm - 9 am";
         parkingInfo.rate = "$0.00";
       }
     } else if (currentDay === 6) {
-      // If the day is Sat
+      // Sat
       parkingInfo.day = "Sat: ";
 
       if (currentHours >= 9 && currentHours < 18) {
-        // If the time is between 9 am and 6 pm
+        // Sat: Between 9 am and 6 pm
         parkingInfo.time = "9 am - 6 pm";
         parkingInfo.rate = "$" + parking.r_sa_9a_6;
       } else if (currentHours >= 18 && currentHours < 22) {
-        // If the time is between 6 pm and 10 pm
+        // Sat: Between 6 pm and 10 pm
         parkingInfo.time = "6 pm - 10 pm";
         parkingInfo.rate = "$" + parking.r_sa_6p_10;
       } else {
-        // If the time is between 10 pm and 9 am
+        // Sat: Between 10 pm and 9 am
         parkingInfo.time = "10pm - 9 am";
         parkingInfo.rate = "$0.00";
       }
     } else {
-      // If the day is Sun
+      // Sun
       if (currentHours >= 9 && currentHours < 18) {
-        // If the time is between 9 am and 6 pm
+        // Sun: Between 9 am and 6 pm
         parkingInfo.time = "9 am - 6 pm";
         parkingInfo.rate = "$" + parking.r_su_9a_6;
       } else if (currentHours >= 18 && currentHours < 22) {
-        // If the time is between 6 pm and 10 pm
+        // Sun: Between 6 pm and 10 pm
         parkingInfo.time = "6 pm - 10 pm";
         parkingInfo.rate = "$" + parking.r_su_6p_10;
       } else {
-        // If the time is between 10 pm and 9 am
+        // Sun: Between 10 pm and 9 am
         parkingInfo.time = "10pm - 9 am";
         parkingInfo.rate = "$0.00";
       }
@@ -179,7 +182,6 @@ function HomePage({ setIsLoggedIn , userId}) {
 
     // TODO: redirect to login page if the user is not logged in
     navigate(`/booking/${userId}`);
-    
   };
 
   const handleFilterOptions = (data) => {
@@ -197,7 +199,6 @@ function HomePage({ setIsLoggedIn , userId}) {
               className="search__input"
               placeholder="Search by location or address"
             ></input>
-            {/* TODO: Add an indication that filter is on */}
             <div className="search__filter-cont">
               <img
                 src={filter}
@@ -233,6 +234,7 @@ function HomePage({ setIsLoggedIn , userId}) {
               {!filteredParkingMeters
                 ? ""
                 : filteredParkingMeters.map((parking) => {
+                    const parkingInfo = displayParkingRate(parking);
                     return (
                       <Marker
                         key={parking.meter_id}
@@ -252,15 +254,19 @@ function HomePage({ setIsLoggedIn , userId}) {
                             <p className="pop-up__info">{parking.meterhead}</p>
                             <p className="pop-up__header">METER ID:</p>
                             <p className="pop-up__info">{parking.meter_id}</p>
-                            <p className="pop-up__header">CURRENT RATE:</p>
-                            <p className="pop-up__info">
-                              {displayParkingRate(parking, Date.now()).day}{" "}
-                              {displayParkingRate(parking, Date.now()).time}
-                            </p>
-                            <p className="pop-up__info">
-                              {displayParkingRate(parking, Date.now()).rate} per
-                              hour
-                            </p>
+                            {parking.status === "vacant" ? (
+                              <>
+                                <p className="pop-up__header">CURRENT RATE:</p>
+                                <p className="pop-up__info">
+                                  {parkingInfo.day} {parkingInfo.time}
+                                </p>
+                                <p className="pop-up__info">
+                                  {parkingInfo.rate} per hour
+                                </p>
+                              </>
+                            ) : (
+                              ""
+                            )}
                             <p className="pop-up__header">LOCATION:</p>
                             <p className="pop-up__info">
                               {parking.geo_local_area}
