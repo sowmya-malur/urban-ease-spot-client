e471ada30df7acf41a4ddf86d09248b147ef238a
Sowmya Malur <smalur.github@gmail.com>
2024-04-01 11:39:15 -0700

    Clean up comments and format code

--- src/components/ParkingNotification/ParkingNotification.jsx
+++ src/components/ParkingNotification/ParkingNotification.jsx
@@ -14,68 +14,72 @@ import "../ParkingNotification/ParkingNotification.scss";
 import warningIcon from "../../assets/icons/round_warning_amber_black_24dp.png";
 import backIcon from "../../assets/icons/round_arrow_back_black_24dp.png";
 
-function ParkingNotification({setIsLoggedIn}) {
-
+function ParkingNotification({ setIsLoggedIn }) {
   // Initialize hooks
   const navigate = useNavigate();
 
+  // Get user id from local storage if it exists
   let userId = 0;
-  if(localStorage.getItem("isLoggedIn")) {
+  if (localStorage.getItem("isLoggedIn")) {
     userId = localStorage.getItem("userId");
-    console.log("userId in duration", userId);
   }
 
   // Initialize state variables
   const [expireSoon, setExpireSoon] = useState(false);
   const [bookingData, setBookingData] = useState(null);
 
-   // Set the isloggedIn state variable from localStorage on mount
-   useEffect(() => {
+  // Set the isloggedIn state variable from localStorage on mount
+  useEffect(() => {
     setIsLoggedIn(localStorage.getItem("isLoggedIn"));
-    if(localStorage.getItem("isLoggedIn") !== "true") {
+    if (localStorage.getItem("isLoggedIn") !== "true") {
       navigate("/login");
     }
   }, []);
 
-  useEffect(()=>{ 
-    const getActiveBooking = async() => {
-
+  // Get active bookings for the user id to display
+  // If there are no active bookings, display default message
+  useEffect(() => {
+    const getActiveBooking = async () => {
       try {
-        if(localStorage.getItem("isLoggedIn")) {
-          const response = await axios.get(`http://localhost:8080/api/booking/user/${userId}`);
-          console.log("response",response.data);
-          if(response.data && response.status === 200) {
+        if (localStorage.getItem("isLoggedIn")) {
+          const response = await axios.get(
+            `http://localhost:8080/api/booking/user/${userId}`
+          );
+
+          if (response.data && response.status === 200) {
             setBookingData(response.data);
             setExpireSoon(true);
           }
         }
-      } catch(error) {
+      } catch (error) {
         console.error("Error retrieving active booking:", error);
       }
-     
     };
 
     // call to async func
     getActiveBooking();
-  },[userId]);
+  }, [userId]);
 
-  const handleEndSession = async() => {
-try {
-  const response = await axios.put(`http://localhost:8080/api/booking/${bookingData.meter_id}/user/${userId}`, 
-    { 
-      id: bookingData.id,
-      status: "complete"
-  });
+  // Update the active booking to complete and release the parking spot
+  const handleEndSession = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:8080/api/booking/${bookingData.meter_id}/user/${userId}`,
+        {
+          id: bookingData.id,
+          status: "complete",
+        }
+      );
 
-    if(response.data && response.status === 200){
-      alert("Parking session ended successfully.");
-      setBookingData(null);
-      setExpireSoon(false);
-      navigate("/");
+      if (response.data && response.status === 200) {
+        alert("Parking session ended successfully.");
+        setBookingData(null);
+        setExpireSoon(false);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error ending the parking session", error);
     }
-} catch (error) {
-  console.error("Error ending the parking session");
-}   
   };
 
   const handleDismiss = () => {
@@ -84,57 +88,70 @@ try {
 
   return (
     <main>
+      {/* Redirect user to Login page if user is not logged in */}
       {!localStorage.getItem("isLoggedIn") ? (
-        <LoginPage setIsLoggedIn={setIsLoggedIn} /> 
+        <LoginPage setIsLoggedIn={setIsLoggedIn} />
       ) : (
-      <section className="notification">
-        <div className="notification__page-header">
-          <img
-            className="notification__icon"
-            src={backIcon}
-            onClick={handleDismiss}
-            alt="back-icon"
-          />
-          <h1 className="notification__title">Notification</h1>
-        </div>
-        {bookingData && expireSoon && (
-          <>
-          <div className="notification__meter-info">
-          <p>Meter #</p>
-          <h2 className="notification__sub-title">{bookingData?.meter_id}</h2>
-          <p>{bookingData?.location}</p>
-        </div>
-        <div className="notification__container notification__container--top-spacing">
-              <img
-                src={warningIcon}
-                alt="warning-icon"
-                className="notification__icon notification__icon--warning"
-              />
-              <p className="notification__info">
-                Your parking will expire at
-              </p>
-              <p className="notification__time">{formatDateToLocale(bookingData?.end_time)}</p>
-              <p className="notification__info">
-                Please remove your car to avoid penalties.
-              </p>
-            </div>
-            <div className="notification__wrapper">
-              <button className="notification__secondary" onClick={handleDismiss}>Dismiss</button>
-              <button className="notification__cta" onClick={handleEndSession}>
-                End Parking Session
-              </button>
-            </div>
-          </>
-          
-        )}
-        {!bookingData && !expireSoon && (
-          <p className="notification__default">
-            Currently, there are no active parking sessions associated with
-            this account.
-          </p>
-        )}
-    
-      </section>
+        <section className="notification">
+          <div className="notification__page-header">
+            <img
+              className="notification__icon"
+              src={backIcon}
+              onClick={handleDismiss}
+              alt="back-icon"
+            />
+            <h1 className="notification__title">Notification</h1>
+          </div>
+          {/* Display details of active booking if it exists */}
+          {bookingData && expireSoon && (
+            <>
+              <div className="notification__meter-info">
+                <p>Meter #</p>
+                <h2 className="notification__sub-title">
+                  {bookingData?.meter_id}
+                </h2>
+                <p>{bookingData?.location}</p>
+              </div>
+              <div className="notification__container notification__container--top-spacing">
+                <img
+                  src={warningIcon}
+                  alt="warning-icon"
+                  className="notification__icon notification__icon--warning"
+                />
+                <p className="notification__info">
+                  Your parking will expire at
+                </p>
+                <p className="notification__time">
+                  {formatDateToLocale(bookingData?.end_time)}
+                </p>
+                <p className="notification__info">
+                  Please remove your car to avoid penalties.
+                </p>
+              </div>
+              <div className="notification__button-cont">
+                <button
+                  className="notification__secondary"
+                  onClick={handleDismiss}
+                >
+                  Dismiss
+                </button>
+                <button
+                  className="notification__cta"
+                  onClick={handleEndSession}
+                >
+                  End Parking Session
+                </button>
+              </div>
+            </>
+          )}
+          {/* Display default message if there are no active bookings for the user */}
+          {!bookingData && !expireSoon && (
+            <p className="notification__default">
+              Currently, there are no active parking sessions associated with
+              this account.
+            </p>
+          )}
+        </section>
       )}
     </main>
   );
